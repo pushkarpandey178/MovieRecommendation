72205d7cd6659e3c258a912ad889a29829acb0ec
pushkar pandey <130895906+pushkarpandey178@users.noreply.github.com>
2023-08-19 01:48:35 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,54 @@
 import streamlit as st
 import pickle
+import pandas as pd
+import requests
 
-movies_list=pickle.load(open('movies.pkl','rb'))
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=1ba323eb8c6b5a0627715b36b8128b71&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
-st.title('Movie Recommender System')
-import streamlit as st
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    recommended_movies = []
+    recommended_movies_posters = []
+    for i in movie_list:
+        movie_id = movies.iloc[i[0]].movie_id
 
-option = st.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
+        recommended_movies.append(movies.iloc[i[0]].title)
+        # fetch poster from API
+        recommended_movies_posters.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_posters
+
+movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.title('     Movie Recommender System ')
+
+selected_movie_name = st.selectbox(
+    'Select a movie:',
+    movies['title'].values)
+
+if st.button('Recommend'):
+    names, posters = recommend(selected_movie_name)
+
+    cols = st.columns(5)
+    with cols[0]:
+        st.text(names[0])
+        st.image(posters[0])
+    with cols[1]:
+        st.text(names[1])
+        st.image(posters[1])
+    with cols[2]:
+        st.text(names[2])
+        st.image(posters[2])
+    with cols[3]:
+        st.text(names[3])
+        st.image(posters[3])
+    with cols[4]:
+        st.text(names[4])
+        st.image(posters[4])
